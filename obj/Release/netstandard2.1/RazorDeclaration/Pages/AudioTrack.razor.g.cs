9c0a6227c5886a6ab3106b3adeffb2a8f55bb906
Dakota Brant <Dakota Brant>
2020-11-28 17:17:16 -0600

    Make link to GitHub more mobile frendly.

--- obj/Release/netstandard2.1/RazorDeclaration/Pages/AudioTrack.razor.g.cs
+++ obj/Release/netstandard2.1/RazorDeclaration/Pages/AudioTrack.razor.g.cs
@@ -13,70 +13,70 @@ namespace CodeUhSite.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 1 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 2 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 3 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 4 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 5 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 6 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 7 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 8 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using CodeUhSite;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 9 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using CodeUhSite.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\dev\CodeUhSite\CodeUhSite\_Imports.razor"
+#line 10 "C:\dev\CodeUhsBlazorAudioPlayer\_Imports.razor"
 using CodeUhSite.Models;
 
 #line default
@@ -90,7 +90,7 @@ using CodeUhSite.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 3 "C:\dev\CodeUhSite\CodeUhSite\Pages\AudioTrack.razor"
+#line 3 "C:\dev\CodeUhsBlazorAudioPlayer\Pages\AudioTrack.razor"
        
     [Parameter] public AudioTrackData AudioTrackData { get; set; }
     [Parameter] public EventCallback<AudioTrackData> TrackSelected { get; set; }
