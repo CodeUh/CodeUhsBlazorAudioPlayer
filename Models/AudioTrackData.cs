4e6b0039bc3d6fcf50073f0ac93102246880da4b
Dakota Brant <Dakota Brant>
2020-11-28 16:38:36 -0600

    Initial commit of what I have deployed currently.

---
+++ Models/AudioTrackData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeUhSite.Models
+{
+    public class AudioTrackData
+    {
+        public string TrackName { get; set; }
+
+        public string TrackFile { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
